fea2d28c8783d8bfc25375f0fb375eb6d87fe0d4
gsrhax <151883647+gsrhax@users.noreply.github.com>
2023-12-26 20:50:21 -0500

    Create traction.js

---
+++ traction.js
@@ -0,0 +1,19 @@
+/*
+
+This is the traction file, bunch of hasthtags, you shouldn't really be reading this, I don't even know why you would lmaooo, but seriously... byee
+
+
+
+NGL, Link, ngl-link, ngl-app, ngl-clone, phishing-tool, steal-passwords, instagram-login, instagram-phish, instagram-hax, instagram-tool, phishing, phish-tool, phisher, phish-link, phishing-link, phishing-link-gen, link-generator, hack-link
+App,Story,Insta,Phishing,Hacking,Tool,NGLClone,NGLPhisher,PhishApp,HaxTool,NGLStoryApp,InstagramHacker,PasswordStealer,PhishLinkGen,PhishLinkTool,NGLAppTool,StoryHacker,InstaPhishTool,PhishGen,AppHacking,StoryPhisher
+,InstaLinkGen,NGLHacker,LinkPhisher,AppPhishing,ToolHacking,StoryLinkGen,InstagramTool,PasswordHack,HackingLink,NGLPhishing,PhishingApp,AppLinkGen,ToolPhisher,StoryHackLink,InstaPhishLink ,LinkAppHack
+
+
+
+
+
+
+
+.... ummm, ur still here??? wth, well I hope you enjoyed reading those keywords/hastags (ik there's no actual hashes 🙄)
+
+byeeee */
